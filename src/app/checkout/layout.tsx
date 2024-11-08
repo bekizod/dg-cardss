838e3769f30e67cda947e8494fb44830f3de51e2
Bekizod <bekizodcancer@gmail.com>
2024-11-08 12:45:29 +0300

    updating order summary card

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,16 +1,15 @@
 // app/checkout/layout.tsx
 "use client";
-
 import { useEffect, useRef, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { FaTrashAlt } from "react-icons/fa";
+// import { FaTrashAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, AppDispatch } from '../../redux/store';
-import { removeFromCart, incrementQuantity, decrementQuantity } from '../../redux/slices/cartSlice';
-import { message } from 'antd';
+// import { removeFromCart, incrementQuantity, decrementQuantity } from '../../redux/slices/cartSlice';
+// import { message } from 'antd';
 import { useAuth } from "@/context/UserContext";
 import Cookies from 'js-cookie'; 
 import { useRouter } from "next/navigation";
@@ -34,8 +33,8 @@ interface Product {
  
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   const [cartEmpty, setCartEmpty] = useState(false); // Assuming cart state
-  const cartRef = useRef<HTMLDivElement | null>(null);
-  const [quantity, setQuantity] = useState(1);
+  // const cartRef = useRef<HTMLDivElement | null>(null);
+  // const [quantity, setQuantity] = useState(1);
   const [showCouponFields, setShowCouponFields] = useState(false);
   const { user } = useAuth();
 const token = Cookies.get("token");
@@ -44,7 +43,7 @@ const dispatch = useDispatch<AppDispatch>();
 const [isMounted, setIsMounted] = useState(false);
 const { totalQuantity, totalPrice, totalDiscount } = useSelector((state: RootState) => state.cart);
 // Ensure component is mounted before rendering cart items (client-side rendering)
-const [filteredCartItems, setFilteredCartItems] = useState(cartItems);
+const [filteredCartItems, setFilteredCartItems] = useState<any>(null);
 const router = useRouter();
   
  useEffect(() => {
@@ -119,7 +118,7 @@ const router = useRouter();
               </div>
               <div className="flex flex-col items-center">
                 <Image src="/cart.gif" alt="empty cart icon" width={250} height={250} className="mb-4" />
-                <p className="text-center text-gray-600 dark:text-gray-400 mb-4">Your cart is empty. Add your favorite products to it.</p>
+                <div className="text-center text-gray-600 dark:text-gray-400 mb-4">Your cart is empty. Add your favorite products to it.</div>
                 <Link href="/SA_en" passHref>
                   <motion.button whileTap={{ scale: 0.95 }} className=" bg-[var(--color-primary)] text-white py-2 px-4 rounded-lg hover:bg-blue-600 dark:hover:bg-blue-500">
                     Start Shopping
@@ -146,7 +145,7 @@ const router = useRouter();
                     </div>
 
                     {/* Bottom Label */}
-                    <p className={`text-sm mt-2 font-medium ${activeStep >= step.id ? "text-blue-600 dark:text-blue-400" : "text-gray-500 dark:text-gray-400"}`}>{step.name}</p>
+                    <div className={`text-sm mt-2 font-medium ${activeStep >= step.id ? "text-blue-600 dark:text-blue-400" : "text-gray-500 dark:text-gray-400"}`}>{step.name}</div>
                   </div>
                 ))}
               </div>
@@ -160,10 +159,10 @@ const router = useRouter();
             {/* Cart Products */}
 
             {/* Order Summary Title */}
-            <h2 className="text-xl font-semibold dark:text-gray-200">Order Summary</h2>
+            <div className="text-xl font-semibold dark:text-gray-200">Order Summary</div>
 
            <div>
-            {filteredCartItems.map((item) => (
+            {filteredCartItems?.map((item : any) => (
               <div className="flex items-center mb-4 " key={item.id}>
                  
                   <Link href={`/singleProduct/${item.name}/${item.id}`} className="flex-shrink-0">
@@ -177,14 +176,34 @@ const router = useRouter();
                   </Link>
             <div className="ml-4 flex flex-col justify-between">
             <div>
-              <Link href= "/#" >
-                <p className="text-sm font-semibold">{item.name}</p>
-                <p>Color: {item.color}</p>
-                <p>Quantity: {item.quantity}</p>
-              </Link>
+        
+                <div className="text-sm font-semibold">{item.name}</div>
+                <div>Color: {item.color}</div>
+                <div>Quantity: {item.quantity}</div>
+              
             </div>
-            <div className="text-sm font-bold text-green-600">{item.price}</div>
-            <p className="text-sm text-gray-500">Tax included</p>
+             
+            {
+              item.discount > 0 ? (<>
+              <div className="flex items-center text-red-600 dark:text-red-400">
+              <div className="flex flex-col gap-4">
+              <div className="line-through text-gray-500 dark:text-gray-400">{item.price}</div>
+              <div className="text-lg font-bold text-green-600">{item.unitPrice}</div>
+              
+              </div>
+              <div className="flex w-full justify-end">
+                  <div className="text-lg font-bold">-{Math.round(item.discount)}%</div>
+                  <svg className="ml-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M21.5299 10.87L20.0099 9.35001C19.7499 9.09 19.5399 8.58001 19.5399 8.22001V6.06C19.5399 5.18 18.8199 4.46 17.9399 4.46H15.7899C15.4299 4.46 14.9199 4.25 14.6599 3.99L13.1399 2.47C12.5199 1.85 11.4999 1.85 10.8799 2.47L9.33988 3.99C9.08988 4.25 8.57988 4.46 8.20988 4.46H6.05988C5.17988 4.46 4.45988 5.18 4.45988 6.06V8.21C4.45988 8.57 4.24988 9.08 3.98988 9.34L2.46988 10.86C1.84988 11.48 1.84988 12.5 2.46988 13.12L3.98988 14.64C4.24988 14.9 4.45988 15.41 4.45988 15.77V17.92C4.45988 18.8 5.17988 19.52 6.05988 19.52H8.20988C8.56988 19.52 9.07988 19.73 9.33988 19.99L10.8599 21.51C11.4799 22.13 12.4999 22.13 13.1199 21.51L14.6399 19.99C14.8999 19.73 15.4099 19.52 15.7699 19.52H17.9199C18.7999 19.52 19.5199 18.8 19.5199 17.92V15.77C19.5199 15.41 19.7299 14.9 19.9899 14.64L21.5099 13.12C22.1599 12.51 22.1599 11.49 21.5299 10.87ZM7.99988 9C7.99988 8.45 8.44988 8 8.99988 8C9.54988 8 9.99988 8.45 9.99988 9C9.99988 9.55 9.55988 10 8.99988 10C8.44988 10 7.99988 9.55 7.99988 9ZM9.52988 15.53C9.37988 15.68 9.18988 15.75 8.99988 15.75C8.80988 15.75 8.61988 15.68 8.46988 15.53C8.17988 15.24 8.17988 14.76 8.46988 14.47L14.4699 8.47001C14.7599 8.18001 15.2399 8.18001 15.5299 8.47001C15.8199 8.76 15.8199 9.24 15.5299 9.53L9.52988 15.53ZM14.9999 16C14.4399 16 13.9899 15.55 13.9899 15C13.9899 14.45 14.4399 14 14.9899 14C15.5399 14 15.9899 14.45 15.9899 15C15.9899 15.55 15.5499 16 14.9999 16Z"
+                      fill="#FF233F"
+                    />
+                  </svg>
+                  </div>
+                </div>
+              </>) : (<> <div className="text-sm font-bold text-green-600">{item.price}</div></>)
+            }
+            <div className="text-sm text-gray-500">Tax included</div>
           </div>
                    
              
@@ -233,23 +252,23 @@ const router = useRouter();
             {/* Money Totals */}
             <div className="mt-6 space-y-2 dark:text-gray-300">
                 <div className="flex justify-between">
-                  <p>Subtotal</p>
-                  <p>{totalPrice} SAR</p>
+                  <div>Subtotal</div>
+                  <div>{totalPrice} SAR</div>
                 </div>
                 <div className="flex justify-between text-red-500 dark:text-red-400">
-                  <p>Discount</p>
-                  <p>-{totalDiscount} SAR</p>
+                  <div>Discount</div>
+                  <div>-{totalDiscount} SAR</div>
                 </div>
                 <div className="flex justify-between">
-                  <p>Shipping</p>
-                  <p>0 SAR</p>
+                  <div>Shipping</div>
+                  <div>0 SAR</div>
                 </div>
               </div>
 
               {/* Total */}
               <div className="flex justify-between text-lg font-bold mt-4 dark:text-gray-200">
-                <p>Total</p>
-                <p>{totalPrice} SAR</p>
+                <div>Total</div>
+                <div>{totalPrice} SAR</div>
               </div>
 
             {/* Checkout Button */}
