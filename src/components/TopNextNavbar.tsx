9381ccd744580f5049329a4b9c49ea6781892f50
Bekizod <bekizodcancer@gmail.com>
2024-11-09 13:05:44 +0300

    ...

--- src/components/TopNextNavbar.tsx
+++ src/components/TopNextNavbar.tsx
@@ -444,7 +444,7 @@ export default function TopNextNavbar({ logoUrl }: { logoUrl: string }) {
               </button>
 
               <div className="text-2xl font-semibold mb-4">
-                Search Results <span>Total Products of {total}</span>
+                Search Results <div>Total Products of {total}</div>
               </div>
 
               {status === "loading" && <p>Loading products...</p>}
