4bfaaf62bebb6a7d8c5a82b2f1c223c420178b0b
Bekizod <bekizodcancer@gmail.com>
2024-11-10 11:27:12 +0300

    updating the formatting

--- src/app/account/favorites/page.tsx
+++ src/app/account/favorites/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { fetchFavorites, removeFavorite } from "@/redux/slices/favoriteSlice";
-import { fetchFavoriteProducts, removeFavoriteProduct } from "@/redux/slices/favoriteProductsSlice";
+import {
+  fetchFavoriteProducts,
+  removeFavoriteProduct,
+} from "@/redux/slices/favoriteProductsSlice";
 import { AppDispatch, RootState } from "@/redux/store";
 import { Button, notification } from "antd";
 import Image from "next/image";
@@ -9,14 +12,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { DeleteOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { addToCart, decrementQuantity, incrementQuantity } from "@/redux/slices/cartSlice";
+import {
+  addToCart,
+  decrementQuantity,
+  incrementQuantity,
+} from "@/redux/slices/cartSlice";
 import { useAuth } from "@/context/UserContext";
 import { MdDelete } from "react-icons/md";
 
 const FavoriteList: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { favorites, loading, error } = useSelector((state: RootState) => state.favorites as any);
-  const { favoriteProducts } = useSelector((state: RootState) => state.favoriteProducts as any);
+  const { favorites, loading, error } = useSelector(
+    (state: RootState) => state.favorites as any
+  );
+  const { favoriteProducts } = useSelector(
+    (state: RootState) => state.favoriteProducts as any
+  );
   const cartItems = useSelector((state: RootState) => state.cart.items);
   const { user } = useAuth();
   useEffect(() => {
@@ -50,29 +61,26 @@ const FavoriteList: React.FC = () => {
   };
   const handleRemoveFavProduct = async (productId: string) => {
     try {
-
       await dispatch(removeFavoriteProduct(productId)).unwrap();
       notification.success({
-        message: 'Success',
-        description: 'Product removed from favorites!',
+        message: "Success",
+        description: "Product removed from favorites!",
       });
     } catch (error: any) {
-      let errorMessage = 'Failed to save favorite product.';
+      let errorMessage = "Failed to save favorite product.";
       // Check if the error message contains "jwt malformed"
       if (error == "jwt malformed") {
-        errorMessage = 'Authentication error: Please log in to save favorite products.';
+        errorMessage =
+          "Authentication error: Please log in to save favorite products.";
       } else {
-        errorMessage = 'Failed,No internet connection.';
+        errorMessage = "Failed,No internet connection.";
       }
       notification.error({
-        message: 'Error',
+        message: "Error",
         description: errorMessage,
       });
     }
   };
-
-
-
 
   const handleAddToCart = (product: any) => {
     // Implement the logic to dispatch addToCart action with the product details
@@ -101,7 +109,11 @@ const FavoriteList: React.FC = () => {
   }
 
   if (error) {
-    return <p className="text-center text-2xl font-bold mt-[124px]">Error: {error}</p>;
+    return (
+      <p className="text-center text-2xl font-bold mt-[124px]">
+        Error: {error}
+      </p>
+    );
   }
 
   const cardVariants = {
@@ -111,7 +123,9 @@ const FavoriteList: React.FC = () => {
 
   return (
     <div className="dark:bg-slate-900 py-8 bg-white px-4 max-lg:mt-[32px] lg:mt-[124px]">
-      <h3 className="text-2xl font-bold text-center mb-6 dark:text-white text-black">Your Favorite Categories</h3>
+      <h3 className="text-2xl font-bold text-center mb-6 dark:text-white text-black">
+        Your Favorite Categories
+      </h3>
       {favorites?.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {favorites.map((category: any) => (
@@ -122,7 +136,9 @@ const FavoriteList: React.FC = () => {
               animate="visible"
               className="rounded-lg shadow-lg dark:bg-slate-800 dark:border-slate-700 bg-white border border-gray-200 p-4 flex flex-col items-center"
             >
-              <Link href={`/${category.parentCategory.categoryName}/${category.parentCategory._id}/${category.categoryName}/${category._id}`}>
+              <Link
+                href={`/${category.parentCategory.categoryName}/${category.parentCategory._id}/${category.categoryName}/${category._id}`}
+              >
                 <Image
                   src={category.categoryLogo?.data as string}
                   alt={category.categoryName}
@@ -131,7 +147,9 @@ const FavoriteList: React.FC = () => {
                   className="rounded-lg object-cover w-full h-40"
                 />
               </Link>
-              <h4 className="mt-4 text-lg font-semibold text-center text-red-400">{category.categoryName}</h4>
+              <h4 className="mt-4 text-lg font-semibold text-center text-red-400">
+                {category.categoryName}
+              </h4>
               <Button
                 type="primary"
                 danger
@@ -146,88 +164,153 @@ const FavoriteList: React.FC = () => {
         </div>
       ) : (
         <div className="text-gray-500  text-center flex justify-center">
-        <Image src={"/fav_gif.gif"} className="h-72 w-72 rounded" alt={"order"} width={1000} height={1000}/>
-      </div>
+          <Image
+            src={"/fav_gif.gif"}
+            className="h-72 w-72 rounded"
+            alt={"order"}
+            width={1000}
+            height={1000}
+          />
+        </div>
       )}
 
-      <h3 className="text-2xl font-bold text-center my-8 dark:text-white text-black">Your Favorite Products</h3>
+      <h3 className="text-2xl font-bold text-center my-8 dark:text-white text-black">
+        Your Favorite Products
+      </h3>
+
       {favoriteProducts?.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 px-6 lg:grid-cols-4 gap-6">
           {favoriteProducts.map((product: any) => {
-
-
             const productIdt = product?._id as any;
             const buyerId = user?._id || "guest";
-            const productColor = product?.additionalInformation?.color || "default";
+            const productColor =
+              product?.additionalInformation?.color || "default";
             // Check if the product is already in the cart
-            const existingItem = cartItems.find((item) => item.id === productIdt && item.buyerId === buyerId && item.color === productColor);
+            const existingItem = cartItems.find(
+              (item) =>
+                item.id === productIdt &&
+                item.buyerId === buyerId &&
+                item.color === productColor
+            );
             const existingQuantity = existingItem ? existingItem.quantity : 0;
             const BuyerId = existingItem ? existingItem.buyerId : "guest";
             const ID = existingItem ? existingItem.id : "";
-            const isFavorite = favoriteProducts?.some((favProduct: any) => favProduct._id === productIdt);
+            const isFavorite = favoriteProducts?.some(
+              (favProduct: any) => favProduct._id === productIdt
+            );
             return (
               <div key={product._id} className="relative   flex-shrink-0">
                 <div className="bg-white dark:bg-slate-700   p-4 rounded-lg shadow-lg">
-                  <div onMouseDown={(e) => e.preventDefault()} onClick={(e) => e.preventDefault()}>
                   <div
-                        onClick={() => handleRemoveFavProduct(product._id)}
-                        className="  text-red-500 cursor-pointer hover:text-red-700 transition-colors duration-300"
-                        aria-label="Remove from favorites"
-                      >
-                        <MdDelete size={27} />
-                      </div>
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <div
+                      onClick={() => handleRemoveFavProduct(product._id)}
+                      className="  text-red-500 cursor-pointer hover:text-red-700 transition-colors duration-300"
+                      aria-label="Remove from favorites"
+                    >
+                      <MdDelete size={27} />
+                    </div>
                     <motion.div
                       className="z-30 pb-1 cursor-pointer"
-
                       whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
+                    ></motion.div>
+
+                    <Link
+                      href={`/singleProduct/${product?.category?.parentCategory?.categoryName}/${product?.category?.parentCategory?._id}/${product?.category?.categoryName}/${product?.category?._id}/${product?.name}/${product?._id}`}
                     >
-
-                      
-                    </motion.div>
-                    
-                    <Link href={`/singleProduct/${product?.category?.parentCategory?.categoryName}/${product?.category?.parentCategory?._id}/${product?.category?.categoryName}/${product?.category?._id}/${product?.name}/${product?._id}`}  >
-                      <Image src={product?.signedUrls[0]} alt={product.name} width={200} height={200} loading="eager" fetchPriority="high" className="w-full h-40  rounded-xl object-cover" />
-                      {
-                        product?.discount > 0 && <p className="absolute top-0 right-0 c text-white text-xs sm:text-sm bg-[var(--color-primary)] font-bold text-center p-1 sm:p-2 rounded-bl-lg rounded-tr-lg z-20">{Math.round(product.discountPercentage)}% SAVE</p>
-                      }
-                      <h2 className="mt-2 text-lg font-semibold">{product.name}</h2>
-                      <p className="text-sm text-gray-600 dark:text-gray-300">{product.additionalInformation.brand}</p>
+                      <Image
+                        src={product?.signedUrls[0]}
+                        alt={product.name}
+                        width={200}
+                        height={200}
+                        loading="eager"
+                        fetchPriority="high"
+                        className="w-full h-40  rounded-xl object-cover"
+                      />
+                      {product?.discount > 0 && (
+                        <p className="absolute top-0 right-0 c text-white text-xs sm:text-sm bg-[var(--color-primary)] font-bold text-center p-1 sm:p-2 rounded-bl-lg rounded-tr-lg z-20">
+                          {Math.round(product.discountPercentage)}% SAVE
+                        </p>
+                      )}
+                      <h2 className="mt-2 text-lg font-semibold">
+                        {product.name}
+                      </h2>
+                      <p className="text-sm text-gray-600 dark:text-gray-300">
+                        {product.additionalInformation.brand}
+                      </p>
                       <div className="mt-2">
-                        {
-                          product?.discount ? (<><p className="text-xl  font-bold text-green-500">{product.discount}</p>
-                            <p className="text-sm line-through text-gray-500">{product.price}</p>
-                            <p className="text-sm text-red-500">SAVE {product.price - product.discount}</p></>) : (<p className="py-5 text-xl font-bold text-green-500">{product.price}</p>)
-                        }
-
+                        {product?.discount ? (
+                          <>
+                            <p className="text-xl  font-bold text-green-500">
+                              {product.discount}
+                            </p>
+                            <p className="text-sm line-through text-gray-500">
+                              {product.price}
+                            </p>
+                            <p className="text-sm text-red-500">
+                              SAVE {product.price - product.discount}
+                            </p>
+                          </>
+                        ) : (
+                          <p className="py-5 text-xl font-bold text-green-500">
+                            {product.price}
+                          </p>
+                        )}
                       </div>
-
                     </Link>
                     {existingItem ? (
                       <div className="flex flex-row items-center justify-center py-2 gap-2">
-                        <button onClick={() => dispatch(decrementQuantity({ id: ID, buyerId: BuyerId }))} className="px-2 py-1 bg-gray-200 dark:bg-gray-600 rounded">
+                        <button
+                          onClick={() =>
+                            dispatch(
+                              decrementQuantity({ id: ID, buyerId: BuyerId })
+                            )
+                          }
+                          className="px-2 py-1 bg-gray-200 dark:bg-gray-600 rounded"
+                        >
                           -
                         </button>
-                        <div className=" text-gray-600 dark:text-gray-300">{existingQuantity}</div>
-                        <button onClick={() => dispatch(incrementQuantity({ id: ID, buyerId: BuyerId }))} className="px-2 py-1 bg-gray-200 dark:bg-gray-600 rounded">
+                        <div className=" text-gray-600 dark:text-gray-300">
+                          {existingQuantity}
+                        </div>
+                        <button
+                          onClick={() =>
+                            dispatch(
+                              incrementQuantity({ id: ID, buyerId: BuyerId })
+                            )
+                          }
+                          className="px-2 py-1 bg-gray-200 dark:bg-gray-600 rounded"
+                        >
                           +
                         </button>
                       </div>
                     ) : (
-                      <motion.button whileTap={{ scale: 0.95 }} onClick={() => handleAddToCart(product)} className="mt-2 w-full  bg-[var(--color-primary)] dark:bg-green-700 text-white font-bold text-xs sm:text-sm py-1 sm:py-2 rounded-xl">
+                      <motion.button
+                        whileTap={{ scale: 0.95 }}
+                        onClick={() => handleAddToCart(product)}
+                        className="mt-2 w-full  bg-[var(--color-primary)] dark:bg-green-700 text-white font-bold text-xs sm:text-sm py-1 sm:py-2 rounded-xl"
+                      >
                         Add to Cart
                       </motion.button>
                     )}
                   </div>
-
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       ) : (
         <div className="text-gray-500  text-center flex justify-center">
-        <Image src={"/fav_gif.gif"} className="h-72 w-72 rounded" alt={"order"} width={1000} height={1000}/>
-      </div>
+          <Image
+            src={"/fav_gif.gif"}
+            className="h-72 w-72 rounded"
+            alt={"order"}
+            width={1000}
+            height={1000}
+          />
+        </div>
       )}
     </div>
   );
